b46a817c8eae579795fa2d23e7be13dddc7dd36f
Denys-Saveliev <90144527+Denys-Saveliev@users.noreply.github.com>
2022-05-27 12:35:58 +0300

    adding Statistics.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 import user from '../data/user.json';
 import Profile from './profile/Profile';
+import Statistics from './statistics/Statistics';
+import data from '../data/data.json';
 
 export default function App() {
   return (
-    <Profile
-      avatar={user.avatar}
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    />
+    <>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+
+      <Statistics stats={data} title={'Upload stats'} />
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
 import './index.css';
 import './components/profile/Profile.jsx';
+import './components/statistics/Statistics.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
